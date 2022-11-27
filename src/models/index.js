fe9ba7a26412dc4e29f965432288f0dee4fa99a2
Narayanaintraedge <pnarayana@intraedge.com>
2022-11-27 20:43:26 +0530

    modified few configs

--- src/models/index.js
+++ src/models/index.js
@@ -4,13 +4,14 @@ const dbConfigs = require("../configs/db_config");
 
 const dbConnect = new Sequelize(dbConfigs.DB, dbConfigs.USER, dbConfigs.PASSWORD, {
     host : dbConfigs.host ,
+    port : dbConfigs.port,
     dialect : dbConfigs.dialect
 });
 
 const testConnect = async()=> {
     try {
         await dbConnect.authenticate();
-        console.log("connectd successfully");
+        console.log("connected successfully");
     } catch(error) {
         console.log("connection failed" , error);
     }
--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ app.get('/', function(req, res){
     res.send('Hello World');
 })
 
+// to start server -> npm run start
 var server = app.listen(8081, function(){
     var host = server.address().address
     var port = server.address().port
